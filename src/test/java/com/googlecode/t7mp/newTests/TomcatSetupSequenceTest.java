e31aad5d0f632ab5dcc1670025f4125d6f0e4d56
jbellmann <joerg.bellmann@googlemail.com>
2012-03-20 19:28:25 +0100

    test added

---
+++ src/test/java/com/googlecode/t7mp/newTests/TomcatSetupSequenceTest.java
@@ -0,0 +1,42 @@
+package com.googlecode.t7mp.newTests;
+
+import java.io.File;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.googlecode.t7mp.BaseConfiguration;
+import com.googlecode.t7mp.TomcatArtifact;
+import com.googlecode.t7mp.configuration.ChainedArtifactResolver;
+import com.googlecode.t7mp.steps.DefaultContext;
+import com.googlecode.t7mp.steps.StepSequence;
+import com.googlecode.t7mp.steps.deployment.TomcatSetupSequence;
+
+/**
+ * 
+ * @author jbellmann
+ *
+ */
+public class TomcatSetupSequenceTest {
+    
+    private final String systemPathToAnTomcatZip = "/home/jbellmann/Downloads/apache-tomcat-7.0.26.zip";
+
+    @Rule
+    public static TemporaryFolder tempFolders = new TemporaryFolder();
+    
+    @Test
+    public void testTomcatSetupSequence(){
+        BaseConfiguration configuration = new BaseConfiguration();
+        TomcatArtifact tomcatArtifact = new TomcatArtifact();
+        tomcatArtifact.setSystemPath(systemPathToAnTomcatZip);
+        File catalinaBase = tempFolders.newFolder("catalinaBase");
+        configuration.setCatalinaBase(catalinaBase);
+        configuration.setTomcatArtifact(tomcatArtifact);
+        DefaultContext context = new DefaultContext(new ChainedArtifactResolver(), configuration);
+        StepSequence sequence = new TomcatSetupSequence();
+        sequence.execute(context);
+        System.out.println(catalinaBase.getAbsolutePath());
+    }
+    
+}
