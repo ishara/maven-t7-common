06d50547f51e27aee7c537e048d130c9d6312f39
jbellmann <joerg.bellmann@googlemail.com>
2012-03-20 19:33:59 +0100

    fixed, CheckT7.. Task was removed

--- src/test/java/com/googlecode/t7mp/steps/deployment/TomcatSetupSequenceTest.java
+++ src/test/java/com/googlecode/t7mp/steps/deployment/TomcatSetupSequenceTest.java
@@ -20,7 +20,6 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.googlecode.t7mp.maven.CheckT7ArtifactsStep;
 import com.googlecode.t7mp.steps.Step;
 import com.googlecode.t7mp.steps.resources.ConfigFilesSequence;
 import com.googlecode.t7mp.steps.resources.CopyConfigResourcesFromClasspathSequence;
@@ -32,20 +31,18 @@ public class TomcatSetupSequenceTest {
     @Test
     public void testTomcatSetupSequence(){
         TestTomcatSetupSequence sequence = new TestTomcatSetupSequence();
-        Assert.assertEquals("7 Steps expected", 7, sequence.getSteps().size());
-        Step two = sequence.getSteps().get(0);
-        Assert.assertTrue(two instanceof CheckT7ArtifactsStep);
-        Step three = sequence.getSteps().get(1);
+        Assert.assertEquals("6 Steps expected", 6, sequence.getSteps().size());
+        Step three = sequence.getSteps().get(0);
         Assert.assertTrue(three instanceof ResolveTomcatStep);
-        Step four = sequence.getSteps().get(2);
+        Step four = sequence.getSteps().get(1);
         Assert.assertTrue(four instanceof CopyConfigResourcesFromClasspathSequence);
-        Step five = sequence.getSteps().get(3);
+        Step five = sequence.getSteps().get(2);
         Assert.assertTrue(five instanceof ConfigFilesSequence);
-        Step six = sequence.getSteps().get(4);
+        Step six = sequence.getSteps().get(3);
         Assert.assertTrue(six instanceof ArtifactDeploymentSequence);
-        Step seven = sequence.getSteps().get(5);
+        Step seven = sequence.getSteps().get(4);
         Assert.assertTrue(seven instanceof WebappSequence);
-        Step eight = sequence.getSteps().get(6);
+        Step eight = sequence.getSteps().get(5);
         Assert.assertTrue(eight instanceof SetSystemPropertiesStep);
     }
     
